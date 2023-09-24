4ccc2c73414dc5ba7b161e17c6d02ab37b93d61e
eduardoesr <eduardoesr@live.com>
2023-09-23 21:11:04 -0300

    Estrutura banco de dados

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class Perfil(models.Model):
+
+    class Categoria(models.TextChoices):
+        PACIENTE = "PACIENTE", _("Paciente")
+        MEDICO = "MEDICO", _("Médico")
+        # VARIAVEL INTERNA DA CLASSE - VALOR A SER REPASSADO - VALOR A SER EXPOSTO
+
+    usuario     = models.OneToOneField(User, on_delete=models.CASCADE, related_name="perfil")
+    nome        = models.TextField()
+    categoria   = models.CharField(max_length=150, choices=Categoria.choices, default=Categoria.PACIENTE)
+
+
--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+import datetime
 
 # Create your models here.
+class Medicamento(models.Model):
+
+    class Unidade(models.TextChoices):
+        ML = "ML", _("Milímetro")
+        COMP = "COMP", _("Comprimido")
+
+    nome        = models.TextField()
+    dose        = models.IntegerField()
+    unidade     = models.CharField(max_length=150, choices=Unidade.choices, default=Unidade.ML)
+    pub_date    = models.DateTimeField(default=datetime.datetime.now)
+
+
+class Receita(models.Model):
+
+    paciente     = models.ForeignKey(User, on_delete=models.CASCADE, related_name="paciente")
+    medico       = models.ForeignKey(User, on_delete=models.CASCADE, related_name="medico")
+    medicamento  = models.ManyToManyField(Medicamento)
+    pub_date     = models.DateTimeField(default=datetime.datetime.now)
